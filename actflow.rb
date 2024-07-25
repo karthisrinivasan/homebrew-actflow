dde4248572662a68506db014c882fec6372b2e10
Karthi Srinivasan <87223671+karthisrinivasan@users.noreply.github.com>
2024-07-25 18:10:34 -0400

    Update actflow.rb

--- actflow.rb
+++ actflow.rb
@@ -23,7 +23,9 @@ class Actflow < Formula
 	system "mv git .git"
 	system "mv gitignore .gitignore"
 	system "mv gitmodules .gitmodules"
-  ENV["CXX"] = HOMEBREW_PREFIX.class.getwd + "/opt/llvm/bin/clang++"
+  # ENV["CXX"] = HOMEBREW_PREFIX.class.getwd + "/opt/llvm/bin/clang++"
+  ENV["CXX"] = "/opt/homebrew/opt/llvm/bin/clang++"
+  # ENV["CXX_COMPILER"] = HOMEBREW_PREFIX.class.getwd + "/opt/llvm/bin/clang++"
   ENV["ACT_HOME"] = prefix.class.getwd
 	system "git submodule update --init --recursive"
 	system "./build"
