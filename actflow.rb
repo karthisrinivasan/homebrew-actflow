6004fe5f31e355353c7e149d52088f4901d95c61
Karthi Srinivasan <87223671+karthisrinivasan@users.noreply.github.com>
2024-07-25 17:32:49 -0400

    Update actflow.rb

--- actflow.rb
+++ actflow.rb
@@ -20,7 +20,7 @@ class Actflow < Formula
 	system "mv gitignore .gitignore"
 	system "mv gitmodules .gitmodules"
 	system "git submodule update --init --recursive"
-  ENV["ACT_HOME"] = prefix.getwd
+  ENV["ACT_HOME"] = prefix.class.getwd
 	system "./build"
   end
 
