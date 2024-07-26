85afcc1d61b7c27d066f497fa6fe3b040be910c7
Karthi Srinivasan <87223671+karthisrinivasan@users.noreply.github.com>
2024-07-25 21:47:29 -0400

    Update actflow.rb

--- actflow.rb
+++ actflow.rb
@@ -5,6 +5,7 @@ class Actflow < Formula
   desc "Top-level repository for the ACT EDA flow"
   homepage "https://avlsi.csl.yale.edu/act/"
   url "https://github.com/asyncvlsi/actflow.git"
+  branch "main"
   version "main"
   license "GPL-2.0"
 
